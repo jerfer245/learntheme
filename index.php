d0777fde38381ea2ae4992f48794b19c6f6d5dca
jerfer245 <jerfernando245@gmail.com>
2025-04-11 13:20:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Main Template File
+
+LearnTheme
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning Wordpress Theme</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+</body>
+
+</html>
